a1ccdc6545ec4aff01e77bea5262be266eb3ea6d
Uday Sankar Singha <udaycollagebwu@gmail.com>
2026-03-12 17:00:26 +0530

    Implement loop to print list of marks
    Add a loop to print values from a list of marks

---
+++ labeClass.py
@@ -0,0 +1,4 @@
+# Create a list of numbers and write a program using a loop to print all the values in the list one by one.
+mmarks=[12,34,65,80,23]
+for i in range(len(marks)):
+    print(marks[i])
