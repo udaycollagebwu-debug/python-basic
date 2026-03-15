4ac69619ea8001141c38b89e8acbdc028989cc54
Uday Sankar Singha <udaycollagebwu@gmail.com>
2026-03-15 22:20:46 +0530

    Add files via upload

---
+++ student_details.py
@@ -0,0 +1,4 @@
+name=input("Enter your name :")
+collage=input("Enter your collage name :")
+semister=input("Enter your streem :")
+print("Name :",name,"\nCollage Name :",collage,"\nSemister :",semister)
